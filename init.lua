417cbc7aadec3a723c3c5c6ba93100ae82b4f2b0
ht3aa <ht3aa@gmail.com>
2024-09-28 21:30:23 +0300

    auto command to notify me to make commit every time i save a file

--- init.lua
+++ init.lua
@@ -3,3 +3,11 @@ require("config.lazy")
 
 -- to make tailwind works properly
 require("lspconfig").tailwindcss.setup({})
+
+-- Add LazyGit command on file save
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*",
+  callback = function()
+    vim.notify("Don't forget to commit", vim.log.levels.ERROR)
+  end,
+})
